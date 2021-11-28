d1160719a5611f789936363b05ee4f15b4c6786c
unknown <iiigogor@gmail.com>
2021-11-28 16:52:41 +0300

    init

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+int pricrTicket=5000;
+int mile=20;
+int bm=pricrTicket/mile;
+System.out.println("Вам начислено:"+bm+ " бонусных миль");
+    }
+}
