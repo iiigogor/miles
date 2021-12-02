00fad5b2b9eda7476cd6ee72c2ed153945ca580e
unknown <iiigogor@gmail.com>
2021-12-02 20:27:33 +0300

    init

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-int pricrTicket=5000;
-int mile=20;
-int bm=pricrTicket/mile;
-System.out.println("Вам начислено:"+bm+ " бонусных миль");
+        int pricrTicket = 10000;
+        int mile = 20;
+        int bm = pricrTicket / mile;
+        System.out.println("Вам начислено:" + bm + " бонусных миль");
     }
 }
